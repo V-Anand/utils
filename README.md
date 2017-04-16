90cc412b1dc340474710a72d2bf9ba6776c8ab06
v.anand <vaidyanathana@g.cofc.edu>
2017-04-15 20:34:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# utils
+
+To Manage my local notes
+========================
+Notes entry consists of a section-name, key, and value.
+
+    ./readNotes.rb <notes-file> <add|get|set|del> <section-name>
+
+
---
+++ readNotes.rb
@@ -0,0 +1,150 @@
+#!/usr/bin/ruby
+
+def writeBytes(f, bytes)
+  bytes.each {|b| f.putc b}
+end
+def readInt32(f)
+    (f.readbyte << 24) | (f.readbyte << 16) | (f.readbyte << 8) | (f.readbyte)
+end
+def writeInt32(f, val)
+    writeBytes(f, [ ((val >> 24) & 0XFF), 
+                    ((val >> 16) & 0XFF),
+                    ((val >> 8 ) & 0XFF),
+                    ((val      ) & 0XFF) ])
+end
+def readStr(f, sz)
+   f.read sz
+end
+def writeStr(f, val)
+    writeBytes(f, val.bytes)
+end
+def assertFileExists(f)
+  if not File.exist? f
+    $stderr.puts "File not exists!"
+    exit 1
+  end
+end
+def assertFileFormatOk(f)
+    f.rewind
+    magic = readInt32(f)
+    if not (0XFFFFBABE == magic)
+      $stderr.puts "Wrong file format!"
+      exit 2 
+    end
+end
+def skipFields(keyf, n)
+  while n > 0
+    len = readInt32(keyf)
+    keyf.seek(len, IO::SEEK_CUR)
+    n = n - 1
+  end
+end
+def addFile(keyf)
+  File.open(keyf, File::BINARY | File::CREAT | File::TRUNC | File::RDWR) {|f|
+    writeInt32(f, 0XFFFFBABE)
+  }
+end
+def addSection(name, keyf)
+  $stdout.print "Enter section key for #{name} : "
+  key = $stdin.gets.chomp
+  $stdout.print "Enter section value for #{name} : "
+  value = $stdin.gets.chomp
+  File.open(keyf, File::BINARY | File::RDWR) {|f|
+    assertFileFormatOk(f)
+    writeInt32(f, name.length)
+    writeStr(f, name)
+    writeInt32(f, key.length)
+    writeStr(f, key)
+    writeInt32(f, value.length)
+    writeStr(f, value)
+  }
+end
+
+def checkAddSection(name, keyf)
+  if not File.exist? keyf
+    addFile(keyf)
+  end
+  addSection(name, keyf)
+end
+def checkSetSection(name, keyf)
+  assertFileExists(keyf)
+  addSection(name, keyf)
+end
+
+def checkGetSection(name, keyf)
+  assertFileExists(keyf)
+  $stdout.print "Enter section key for #{name} : "
+  key = $stdin.gets.chomp
+  value = "<NOT FOUND>"
+  File.open(keyf, File::BINARY | File::RDWR) {|f|
+    assertFileFormatOk(f)
+    while not f.eof?
+        lens = readInt32(f)
+        s = readStr(f, lens)
+        if name == s
+          lenk = readInt32(f)
+          k = readStr(f, lenk)
+          if key == k
+            lenv = readInt32(f)
+            value = readStr(f, lenv)
+          else
+            skipFields(f, 1)
+          end
+        else
+          skipFields(f, 2)
+        end
+    end
+  }
+  value
+end
+
+def checkDelSection(name, keyf)
+  assertFileExists(keyf)
+  $stdout.print "Enter section key for #{name} : "
+  key = $stdin.gets.chomp
+  File.open(keyf, File::BINARY | File::RDWR) {|f|
+    assertFileFormatOk(f)
+    while not f.eof?
+        lens = readInt32(f)
+        s = readStr(f, lens)
+        if name == s
+          lenk = readInt32(f)
+          k = readStr(f, lenk)
+          if key == k
+            lenv = readInt32(f)
+            v = "*" * lenv
+            writeStr(f, v)
+          else
+            skipFields(f, 1)
+          end
+        else
+          skipFields(f, 2)
+        end
+    end
+  }
+end
+
+def main(argv)
+  keyf = argv.shift
+  cmd = argv.shift
+  val = argv.shift
+  if "add" == cmd
+    checkAddSection(val, keyf)
+  elsif "set" == cmd
+    checkSetSection(val, keyf)
+  elsif "get" == cmd
+    $stderr.puts checkGetSection(val, keyf)
+  elsif "del" == cmd
+    checkDelSection(val, keyf)
+  end
+end
+
+def usage
+  "Usage: #{$1} <path-to-key-file> <add|get|set|del> <section>"
+end
+
+if 3 == ARGV.length
+  main(ARGV)
+else
+  $stderr.puts usage
+end
